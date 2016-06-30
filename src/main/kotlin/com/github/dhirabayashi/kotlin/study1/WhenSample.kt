329769eb6d1e43cc7a0cba79cbeb94a17b5ede6c
Daiki Hirabayashi <gigabowser@hotmail.co.jp>
2016-06-30 23:12:37 +0900

    fix idiom

--- src/main/kotlin/com/github/dhirabayashi/kotlin/study1/WhenSample.kt
+++ src/main/kotlin/com/github/dhirabayashi/kotlin/study1/WhenSample.kt
@@ -1,13 +1,11 @@
 package com.github.dhirabayashi.kotlin.study1
 
 class WhenSample {
-    fun useWhen(obj: Any): String {
-        return when(obj) {
-            1 -> "One"
-            "Hello" -> "Hello"
-            is Long -> "Long"
-            !is String -> "Not a String"
-            else -> "Unknown"
-        }
+    fun useWhen(obj: Any) = when(obj) {
+        1 -> "One"
+        "Hello" -> "Hello"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown"
     }
 }
